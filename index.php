98431d2b242cbe3c3e84b09d477cfc7e6dcb1851
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2018-08-13 19:42:37 -0300

    Colocado as tags HTML.

--- index.php
+++ index.php
@@ -1,27 +1,34 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-<script src="linedTextArea/jquery-linedtextarea.js"></script>
-<link href="linedTextArea/jquery-linedtextarea.css" type="text/css" rel="stylesheet" />
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Document</title>
+		<link href="linedTextArea/jquery-linedtextarea.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<textarea class="lined" rows="10" cols="60"></textarea>
 
+		<!-- <form action="" >
+		    <input type="text" name="codigo">
 
-<textarea class="lined" rows="10" cols="60"></textarea>
+		    <input type="submit" value="Enviar">
+		</form> -->
+		<p>--> Area de edição</p>
+		<p>--> Logs</p>
+		<p>--> Pilha de tokens</p>
 
-<!-- <form action="" >
-    <input type="text" name="codigo">
+		http://alan.blog-city.com/jquerylinedtextarea.html
 
-    <input type="submit" value="Enviar">
-</form> -->
-<p>--> Area de edição</p>
-<p>--> Logs</p>
-<p>--> Pilha de tokens</p>
-
-http://alan.blog-city.com/jquerylinedtextarea.html
-
-
-
-<script>
-$(function() {
-	$(".lined").linedtextarea(
-		{selectedLine: 1}
-	);
-});
-</script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+		<script src="linedTextArea/jquery-linedtextarea.js"></script>
+		<script>
+			$(function() {
+				$(".lined").linedtextarea(
+					{selectedLine: 1}
+				);
+			});
+		</script>
+	</body>
+</html>
