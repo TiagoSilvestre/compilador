1fd3b0bc03050e219b95b50207455af6c58f8e97
tiagosilvestre <tiago.silvestre@angular-ecommerce.com>
2018-12-06 15:00:32 -0200

    alteracao procedure

--- tabelaDeSimbolos.php
+++ tabelaDeSimbolos.php
@@ -15,6 +15,8 @@ class TabelaDeSimbolos {
     public $procedure = false;
 
     public $saveTipe = false;
+    public $saveParam = false;
+    
     public $variaveisTMP = [];
     public $nivelZero = true;
 
@@ -24,13 +26,29 @@ class TabelaDeSimbolos {
 
 
     public function verifica($value) {
+
+        // ***** ALTERACOES DE NÍVEL *****
+        // BEGIN
+        if($value->sentenca == 'BEGIN' && $this->nivel == 0) {
+            $this->nivel++;
+        }
+        // END
+        if($value->sentenca == 'END') {
+            $this->nivel--;
+        }
+        //PROCEDURE
+        if($value->sentenca == 'PROCEDURE') {
+            $this->nivel++;
+        }  
+
+
         // PROGRAM
     	if($value->sentenca == 'PROGRAM') {
 			$this->program = true;
 		}
         if($value->codigo == 25 && $this->program) {
-            if($this->verificaSeEstaNaTabela($value->sentenca)){
-                $this->printError('Identificador (' . $value->sentenca . ') já declarado');
+            if($this->verificaSeEstaNaTabela($value)){
+                $this->printError('Identificador (' . $value->sentenca . ') já declarado - program');
             }else {
                 array_push($_SESSION['s'], new Simbolo($value->sentenca, 'variavel', 'PROGRAM', $this->nivel));
                 $this->program = false;
@@ -44,8 +62,8 @@ class TabelaDeSimbolos {
 			$this->label = true;
 		}
         if($value->codigo == 25 && $this->label) {
-            if($this->verificaSeEstaNaTabela($value->sentenca) || $this->verificaTMP($value->sentenca)){
-                $this->printError('Identificador (' . $value->sentenca . ') já declarado');
+            if($this->verificaSeEstaNaTabela($value) || $this->verificaTMP($value->sentenca)){
+                $this->printError('Identificador (' . $value->sentenca . ') já declarado - label');
             } else {
 			    array_push($this->variaveisTMP, $value->sentenca);
             }
@@ -65,8 +83,8 @@ class TabelaDeSimbolos {
 			$this->const = true;
 		}
         if($value->codigo == 25 && $this->const) {
-            if($this->verificaSeEstaNaTabela($value->sentenca) || $this->verificaTMP($value->sentenca)){
-                $this->printError('Identificador (' . $value->sentenca . ') já declarado');
+            if($this->verificaSeEstaNaTabela($value) || $this->verificaTMP($value->sentenca)){
+                $this->printError('Identificador (' . $value->sentenca . ') já declarado - const');
             } else {
 			    array_push($this->variaveisTMP, $value->sentenca);
             }
@@ -87,8 +105,8 @@ class TabelaDeSimbolos {
 			$this->var = true;
 		}
 		if($value->codigo == 25 && $this->var) {
-            if($this->verificaSeEstaNaTabela($value->sentenca) || $this->verificaTMP($value->sentenca)){
-                $this->printError('Identificador (' . $value->sentenca . ') já declarado');
+            if($this->verificaSeEstaNaTabela($value) || $this->verificaTMP($value->sentenca)){
+                $this->printError('Identificador (' . $value->sentenca . ') já declarado - var');
             } else {
 			    array_push($this->variaveisTMP, $value->sentenca);
             }
@@ -112,20 +130,29 @@ class TabelaDeSimbolos {
 		}
 
         if($value->codigo == 25 && $this->procedure) {
-            if($this->verificaSeEstaNaTabela($value->sentenca)){
-                $this->printError('Identificador (' . $value->sentenca . ') já declarado');
+            if($this->verificaSeEstaNaTabela($value)){
+                $this->printError('Identificador (' . $value->sentenca . ') já declarado - procedure');
             }else {
-                array_push($_SESSION['s'], new Simbolo($value->sentenca, 'procedure', 'PROCEDURE', $this->nivel));
-                $this->program = false;
-                return;
+                if($this->saveParam){
+                    array_push($_SESSION['s'], new Simbolo($value->sentenca, 'parâmetro', 'PARAMETRO', $this->nivel));
+                } else {
+                    array_push($_SESSION['s'], new Simbolo($value->sentenca, 'procedure', 'PROCEDURE', $this->nivel));
+                    $this->saveParam = true;
+                    return;
+                }
             }
         }
+
+        if($value->sentenca == ';' && $this->procedure) {
+            $this->procedure = false;
+            $this->saveParam = false;
+		}
 
  
 
         // VERIFICAÇAO
-        if($value->codigo == 25 && !$this->var && !$this->label && !$this->const) {
-            if($this->verificaSeEstaNaTabela($value->sentenca)) {
+        if($value->codigo == 25 && !$this->var && !$this->label && !$this->const && !$this->procedure) {
+            if($this->verificaSeEstaNaTabela($value)) {
                 // verifica o nivel
                 if($this->verificaNivel($value->sentenca)) {
                     $this->printError('Identificador (' . $value->sentenca . ') fora de escopo');
@@ -135,29 +162,17 @@ class TabelaDeSimbolos {
             }
         }
 
-        // ***** ALTERACOES DE NÍVEL *****
-        // BEGIN
-        if($value->sentenca == 'BEGIN' && $this->nivel == 0) {
-            $this->nivel++;
-        }
-        // END
-        if($value->sentenca == 'END') {
-            $this->nivel--;
-        }
-        //PROCEDURE
-        if($value->sentenca == 'PROCEDURE') {
-            $this->nivel++;
-        }              
+            
     } 
 
 
     public function verificaSeEstaNaTabela($value) {
         if(!empty($_SESSION['s'])) {
             foreach ($_SESSION['s'] as $key => $val) {
-                if($val->nome === $value){
+                if($val->nome === $value->sentenca && $val->nivel === $value->nivel){
                     return true;
                 }
-            }            
+            }
         }
         return false;
     }
